a4d4346bcefb7e5f751d1f8cc5d2314bb10fffaf
Edwald <edwald@shadowboxer.co>
2023-07-09 15:13:25 +1000

    add render postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,11 +19,15 @@ import { AuthModule } from './users/auth.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(<string>process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
+      // host: process.env.POSTGRES_HOST,
+      // port: parseInt(<string>process.env.POSTGRES_PORT),
+      // username: process.env.POSTGRES_USER,
+      // password: process.env.POSTGRES_PASSWORD,
+      // database: process.env.POSTGRES_DATABASE,
+      url: process.env.POSTGRES_DATABASE_URL,
+      // ssl: {
+      //   ca: process.env.SSL_CERT,
+      // },
       autoLoadEntities: true,
       synchronize: true, // production site should be disabled;
     }),
